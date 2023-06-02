3515dc0c9f5e31e0c0d2164b299a6543e8469c4b
baharmostafaei <120775848+baharmostafaei@users.noreply.github.com>
2023-06-02 13:11:15 +0330

    add crispy form

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'crispy_forms',
+    'crispy_bootstrap4',
+
+
     'accounts', 
     'pages',
 ]
@@ -135,3 +141,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
