ecb2346f6f30a646a4b28aab64657dc4d7d41ecb
baharmostafaei <120775848+baharmostafaei@users.noreply.github.com>
2023-06-05 10:48:36 +0330

    create comment model

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Comment
 
 
 class ProductAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Product)
+admin.site.register(Comment)
