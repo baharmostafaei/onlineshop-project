514d617af7a1b93bfaae14083fdec2df8ae9ff7c
baharmostafaei <120775848+baharmostafaei@users.noreply.github.com>
2023-06-22 19:42:36 +0330

    tabular in line

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Product, Comment
 
+class ProductCommentInLine(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active']
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'datetime_created',)
+    inlines =[
+        ProductCommentInLine,
+    ]
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('product','body' ,'active',)
