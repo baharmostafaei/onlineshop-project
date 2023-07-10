2885b78905b3886482379e147388622b159c72f8
baharmostafaei <120775848+baharmostafaei@users.noreply.github.com>
2023-07-10 12:31:53 +0330

    quantity updates from cart datail

--- cart/views.py
+++ cart/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .cart import Cart
 from products import models
 from .forms import AddToCartProductForm
+
 def cart_detail_view(request):
     cart = Cart(request)
+
+    for item in cart:
+        item['product_update_quantity_form'] = AddToCartProductForm(initial={
+            'quantity': item['quantity'],
+            'inplace': True,
+        })
     return render(request, 'cart/cart_detail.html', {
-        'cart':cart,
+        'cart': cart,
     })
 
 def add_to_cart_view(request, product_id):
@@ -16,7 +23,12 @@ def add_to_cart_view(request, product_id):
     if form.is_valid():
         cleaned_data = form.cleaned_data
         quantity = cleaned_data['quantity']
-        cart.add(product, quantity)
+        cart.add(product, quantity, replace_current_quantity = cleaned_data['inplace'])
 
     return redirect('cart:cart_detail')
 
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(models.Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
